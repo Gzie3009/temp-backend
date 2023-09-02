0617ed56670b0480ce89a389974395755b72e927
Gzie3009 <108942030+Gzie3009@users.noreply.github.com>
2023-09-02 17:59:42 +0530

    added post to some apis

--- routes/OrderRoutes.js
+++ routes/OrderRoutes.js
@@ -4,8 +4,8 @@ const {auth,isAdmin} = require("../middlewares/auth");
 
 const {addAddress ,addItemToOrder, placeOrder,getOrders, addAddressToOrder, getAllAddress, getOrderDeatils, createOrder} = require("../controllers/Order"); 
 
-router.get("/all-addresses",auth,getAllAddress); // tested
-router.get("/order-details",auth,getOrderDeatils); //tested
+router.post("/all-addresses",auth,getAllAddress); // tested
+router.post("/order-details",auth,getOrderDeatils); //tested
 router.post("/add-address",auth,addAddress); // tested
 router.post("/create-order",auth,createOrder); // tested
 router.post("/addAddressToOrder",auth,addAddressToOrder); //tested
